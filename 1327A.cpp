97006133d548821c141e667d36888ac6f733b73e
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-12-25 22:00:31 +0530

    commit

---
+++ 1327A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        if (n >= k*k)
+        {
+            if ((n + k) % 2 == 0)
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
