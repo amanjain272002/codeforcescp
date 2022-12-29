1f7bdbec78f9a1794bd3f9997ab735c3f0416fb7
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-12-29 21:22:05 +0530

    commit

---
+++ 577A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int n, x, count = 0;
+    cin >> n >> x;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (x % i == 0 && x / i <= n)
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
