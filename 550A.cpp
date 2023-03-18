677178eefea054c10d1672691c47aed1e965b067
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-03-18 20:38:20 +0530

    commit

--- 550A.cpp
+++ 550A.cpp
@@ -4,32 +4,56 @@ using namespace std;
 
 int main()
 {
-    string s, r;
-    int s1 = 0, s2 = 0;
+    string s;
     cin >> s;
-    for (int i = 0; i < s.length() - 1;)
+    int p = 0, q = 0, r = 0, t = 0, ind = 0;
+    for (int i = 0; i < s.length() - 1; i++)
     {
-        r = s.substr(i, 2);
-        if (r == "AB" && s1 == 0)
+        if (s[i] == 'A' && s[i + 1] == 'B')
         {
-            s1 = 1;
-            i = i + 2;
-        }
-        else if (r == "BA" && s2 == 0)
-        {
-            s2 = 1;
-            i = i + 2;
-        }
-        else if (s1 == 1 && s2 == 1)
-        {
+            ind = i;
+            p = 1;
             break;
         }
-        else
+    }
+    if (p == 1)
+    {
+        for (int i = ind + 2; i < s.length() - 1; i++)
         {
-            i = i + 1;
+            if (s[i] == 'B' && s[i + 1] == 'A')
+            {
+                q = 1;
+                break;
+            }
         }
     }
-    if (s1 == 1 && s2 == 1)
+    if (q == 0)
+    {
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s[i] == 'B' && s[i + 1] == 'A')
+            {
+                ind = i;
+                r = 1;
+                break;
+            }
+        }
+
+        if (r == 1)
+        {
+            for (int i = ind + 2; i < s.length() - 1; i++)
+            {
+                if (s[i] == 'A' && s[i + 1] == 'B')
+                {
+                    ind = i;
+                    t = 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    if ((p == 1 && q == 1) || (r == 1 && t == 1))
     {
         cout << "YES";
     }
