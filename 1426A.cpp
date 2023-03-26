68b491a6af45f554169010f91e34a6cbb7288d7b
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-03-26 21:12:35 +0530

    commit

---
+++ 1426A.cpp
@@ -0,0 +1,17 @@
+// Floor Number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,x;
+        cin>>n>>x;
+
+    }
+    
+    return 0;
+}
