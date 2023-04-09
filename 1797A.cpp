e45200b90404fef9098462f1b7302c9bf8606f43
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-04-09 11:30:41 +0530

    commit

--- 1797A.cpp
+++ 1797A.cpp
@@ -5,12 +5,12 @@ void solve()
 {
     long long int n, m, x1, x2, y1, y2;
     cin >> n >> m;
-    int sum = 0;
+    int sum = 0, sum1 = 0;
     cin >> x1 >> y1 >> x2 >> y2;
     if (x1 < n && x1 > 1)
     {
         if (y1 > 1 && y1 < m)
-        {       
+        {
             sum = 4;
         }
         else if (y1 == 1 || y1 == m)
@@ -18,7 +18,20 @@ void solve()
             sum = 3;
         }
     }
-    else if (x1 == 1 || x1 == n)
+
+    if (x2 < n && x2 > 1)
+    {
+        if (y2 > 1 && y2 < m)
+        {
+            sum1 = 4;
+        }
+        else if (y2 == 1 || y2 == m)
+        {
+            sum1 = 3;
+        }
+    }
+
+    if (x1 == 1 || x1 == n)
     {
         if (y1 > 1 && y1 < m)
         {
@@ -29,18 +42,20 @@ void solve()
             sum = 2;
         }
     }
-    // else
-    // {
-    //     if (y1 > 1 && y1 < m)
-    //     {
-    //         sum = 3;
-    //     }
-    //     else if (y1 == 1 || y1 == m)
-    //     {
-    //         sum = 2;
-    //     }
-    // }
-    cout << sum << endl;
+
+    if (x2 == 1 || x2 == n)
+    {
+        if (y2 > 1 && y2 < m)
+        {
+            sum1 = 3;
+        }
+        else if (y2 == 1 || y2 == m)
+        {
+            sum1 = 2;
+        }
+    }
+
+    cout << min(sum, sum1) << endl;
 }
 
 int main()
