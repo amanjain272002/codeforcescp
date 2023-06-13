f31d288a3581dff962ce2970cc1235fd4887561c
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-06-13 21:42:44 +0530

    commit

---
+++ 1841A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        if (n%2!=0)
+        {
+            cout<<"Bob"<<endl;
+        }
+        else
+        {
+            
+        }
+        
+    }
+    
+    return 0;
+}
