19f70f1e4c73e100911c65c959d5d40c6a351bfd
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-06-23 22:14:24 +0530

    commit

---
+++ 1793B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#define ll long long int
+using namespace std;
+
+int main(){
+    ll n;
+    cin>>n;
+    while (n--)
+    {
+        ll t;
+        cin>>t;
+        
+    }
+    
+    return 0;
+}
